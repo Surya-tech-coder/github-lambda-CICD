1c53a1f2149fbdd93863c8f287df43f8a47a08e4
Surya-tech-coder <surya.r641111@gmail.com>
2025-12-05 22:10:23 +0530

    Update-4

--- lambda_function.py
+++ lambda_function.py
@@ -1,21 +1,9 @@
 import json
-import pandas as pd
 
 def lambda_handler(event, context):
-    # Create dummy data for demonstration
-    data = {
-        'col1': [1, 2], 
-        'col2': [3, 4]
-    }
-    
-    # Create a pandas DataFrame
-    df = pd.DataFrame(data=data)
-    
-    # Print the DataFrame to the logs
-    print(df)
-    print("Done x1.1 sidd")
+    print("Hello from Lambda!")
     
     return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda! super siddart')
+        "statusCode": 200,
+        "body": json.dumps("Hello from Lambda! Super Siddharth 🚀")
     }
