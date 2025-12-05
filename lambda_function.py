a3c5b0cad062eeec3073272496eb615d7507fbd0
Surya-tech-coder <surya.r641111@gmail.com>
2025-12-05 20:27:27 +0530

    Initial commit

---
+++ lambda_function.py
@@ -0,0 +1,21 @@
+import json
+import pandas as pd
+
+def lambda_handler(event, context):
+    # Create dummy data for demonstration
+    data = {
+        'col1': [1, 2], 
+        'col2': [3, 4]
+    }
+    
+    # Create a pandas DataFrame
+    df = pd.DataFrame(data=data)
+    
+    # Print the DataFrame to the logs
+    print(df)
+    print("Done x1.1")
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
